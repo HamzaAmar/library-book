df81afc0a40e5a0ad2fb673d0d14fd2eac148b91
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-08-15 14:59:32 +0100

    add global styles

--- src/components/add.test.ts
+++
@@ -1,5 +0,0 @@
-import { add } from './add';
-
-test('testing add ', () => {
-  expect(add(2, 3)).toBe(5);
-});
---
+++ src/components/ui/button/button.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const button = () => {
+  return <button></button>;
+};
+
+export default button;
