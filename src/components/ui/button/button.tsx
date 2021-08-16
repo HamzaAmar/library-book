f4e899b9928c0d59e840203fe82c9eeb96a93462
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-08-16 02:11:29 +0100

    fix: button problems and add more styles

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -3,23 +3,49 @@ import { ButtonProps } from './button.type';
 import styles from './button.module.css';
 import cn from 'classnames';
 
-const button = ({
-  children,
-  type = 'button',
-  rounded = false,
-  icon,
-  iconPosition = 'start'
-}: ButtonProps) => {
+const button = (props: ButtonProps) => {
+  const { children, rounded, icon, iconPosition, variant, color, ...rest } =
+    props;
+
+  console.log('this is the value of color', color);
+
   const root = cn(styles.btn, {
-    [styles.rounded]: rounded
+    [styles.rounded]: rounded,
+    [styles.contained]: variant === 'contained',
+    [styles.outline]: variant === 'outline',
+    [styles.text]: variant === 'text',
+    [styles.primary]: color === 'primary',
+    [styles.secondary]: color === 'secondary',
+    [styles.success]: color === 'success',
+    [styles.error]: color === 'error',
+    [styles.warning]: color === 'warning',
+    [styles.transparent]: color === 'transparent'
   });
+  const left = icon && iconPosition === 'start';
+  const right = icon && iconPosition === 'end';
+
   return (
-    <button type={type} className={root}>
+    <button className={root} {...rest}>
       {iconPosition === 'start' && icon && icon}
-      {children}
+      <span
+        className={cn({
+          [styles.leftMargin]: left,
+          [styles.rightMargin]: right
+        })}
+      >
+        {children}
+      </span>
       {iconPosition === 'end' && icon && icon}
     </button>
   );
+};
+
+button.defaultProps = {
+  type: 'button',
+  iconPosition: 'start',
+  rounded: false,
+  variant: 'contained',
+  color: 'primary'
 };
 
 export default button;
--- src/components/ui/button/button.type.ts
+++ src/components/ui/button/button.type.ts
@@ -2,10 +2,20 @@ import React from 'react';
 import { ButtonHTMLAttributes } from 'react';
 
 type Position = 'start' | 'end';
+type Variant = 'contained' | 'outline' | 'text';
+type Color =
+  | 'primary'
+  | 'secondary'
+  | 'success'
+  | 'warning'
+  | 'error'
+  | 'transparent';
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   rounded?: boolean;
   icon?: React.ReactNode;
   iconPosition?: Position;
+  variant: Variant;
+  color: Color;
 }
