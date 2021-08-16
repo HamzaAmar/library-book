906957ecef60aab040bee831a938a6ac45c2ceaf
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-08-16 14:45:36 +0100

    feat: add layout to the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,18 @@
-import { Button } from '@components/ui';
+import { Layout } from '@components/core';
+import { Button, Input } from '@components/ui';
 import type { NextPage } from 'next';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <Button rounded type="button">
-          Search
-        </Button>
-        <Button type="button">Hello</Button>
-      </main>
-    </div>
+    <Layout>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+    </Layout>
   );
 };
 
