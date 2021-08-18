7c76be712cccda49bc0e9a25de993808fc62c73d
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-08-18 13:50:54 +0100

    feat: add about section component

--- src/components/section/about/about.tsx
+++ src/components/section/about/about.tsx
@@ -1,9 +1,38 @@
+import { RightAboutDesign } from '@components/icons';
+import { Button, Input } from '@components/ui';
 import React from 'react';
+import styles from './about.module.css';
 
 const about = () => {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className={styles.container}>
+      <div className={styles.leftSection}>
+        <p>Explore your favorite books</p>
+        <h1 className={styles.heading}>
+          Get Your New <span className={styles.bStyles}>B</span>ook With The
+          Best Price
+        </h1>
+        <div className={styles.inputContainer}>
+          <div className={styles.inputWrapper}>
+            <Input
+              name="search"
+              placeholder="search book , authors , categories"
+              full
+              style={{ border: 0 }}
+            />
+          </div>
+          <Button color="primary" radius="rounded" size="medium">
+            Search
+          </Button>
+        </div>
+      </div>
+
+      <div className={styles.middleSection}>
+        <img src="library.jpg" className={styles.image} />
+      </div>
+      <div className={styles.rightSection}>
+        <RightAboutDesign />
+      </div>
     </div>
   );
 };
