d4c32d63a65f943a8cb5007728c0c307e089066a
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-08-18 14:00:07 +0100

    feat: add plans section components

---
+++ src/components/section/index.ts
@@ -0,0 +1,8 @@
+export { default as About } from './about';
+export { default as BookList } from './bookList';
+export { default as Testimonials } from './testimonials';
+export { default as BookCarousel } from './bookCarousel';
+export { default as Category } from './category';
+export { default as Book } from './book';
+export { default as Plans } from './plans';
+export { default as SmartPhone } from './smartPhone';
